b1a7b4c54e48ba9bcfe05c1c31b6307b1a40673d
Grégory Bassard <gregory.bassard@he-arc.ch>
2025-03-25 13:34:42 +0100

    NEOs_viewer update

--- app/NEOs_viewer.py
+++ app/NEOs_viewer.py
@@ -5,21 +5,31 @@ import dash
 import pandas as pd
 from dash import dcc, html
 from dash.dependencies import Input, Output
-from utils import load_solar_system, load_asteroid
+from utils import load_solar_system, load_asteroid, get_neos_name
 
 fig = go.Figure()
 
 fig = load_solar_system(fig)
 
-fig = load_asteroid(fig, '2025 FC2', '2025-03-23', '2027-05-23', '1d')
+date_max = '2030-01-01'
+date_min = '2020-01-01'
+dist_max = '0.05LD'
+fullname = '1'
+nea_comet = '1'
+
+neos = get_neos_name(date_max, date_min, dist_max, fullname, nea_comet)
+
+for i in range(len(neos)):
+    fig = load_asteroid(fig, neos['des'][i])
+    # fig = load_asteroid_orbit(fig, neos['des'][i], '2025-03-23', '2027-05-23', '1d')
 
 fig.update_layout(
     title='Orbite des Planètes autour du Soleil',
     template='plotly_dark',
     scene = dict(
-        xaxis = dict(visible=False),
-        yaxis = dict(visible=False),
-        zaxis = dict(visible=False)
+        xaxis = dict(range=[-500000000, 500000000], visible=False),
+        yaxis = dict(range=[-500000000, 500000000], visible=False),
+        zaxis = dict(range=[-500000000, 500000000], visible=False)
         )
     )
 
--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,8 @@
 from skyfield.api import load
 import plotly.graph_objects as go
 from astroquery.jplhorizons import Horizons
+import requests
+import pandas as pd
 
 def load_solar_system(fig:go.Figure)->go.Figure:
     # Charger les éphémérides DE440
@@ -22,7 +24,7 @@ def load_solar_system(fig:go.Figure)->go.Figure:
 
     planet_to_show = ['Mercure', 'Vénus', 'Terre', 'Mars']#, 'Jupiter', 'Saturne', 'Uranus', 'Neptune', 'Pluton']
 
-    for i in range(len(planets_data['name'])):
+    for i in range(len(planets_data)):
         if planets_data['name'][i] not in planet_to_show:
             continue
         
@@ -50,7 +52,8 @@ def load_solar_system(fig:go.Figure)->go.Figure:
             x=[actual_pos[0]], y=[actual_pos[1]], z=[actual_pos[2]],
             mode='markers+text',
             marker=dict(size=4, color=planets_data['color'][i], opacity=0.9),
-            text=planets_data['name'][i]
+            text=planets_data['name'][i],
+            name=planets_data['name'][i]
         ))
 
     # Ajout du Soleil
@@ -63,7 +66,21 @@ def load_solar_system(fig:go.Figure)->go.Figure:
 
     return fig
 
-def load_asteroid(fig:go.Figure, asteroid_id:str, start_date:str, stop_date:str, step:str)->go.Figure:
+def load_asteroid(fig:go.Figure, asteroid_id:str)->go.Figure:
+    # Récupérer les données actuelles de l'astéroïde
+    obj = Horizons(id=asteroid_id, location='500@10', epochs={'start': '2025-03-24', 'stop': '2025-03-25', 'step': '1d'})
+    vectors = obj.vectors()
+
+    fig.add_trace(go.Scatter3d(
+        x=[vectors['x'][0]*1.496e+8], y=[vectors['y'][0]*1.496e+8], z=[vectors['z'][0]*1.496e+8],
+        mode='markers+text',
+        marker=dict(size=4, color='white', opacity=0.9),
+        text=asteroid_id
+    ))
+
+    return fig
+
+def load_asteroid_orbit(fig:go.Figure, asteroid_id:str, start_date:str, stop_date:str, step:str)->go.Figure:
 
     obj = Horizons(id=asteroid_id, location='500@10', epochs={'start': start_date, 'stop': stop_date, 'step': step})
 
@@ -77,11 +94,12 @@ def load_asteroid(fig:go.Figure, asteroid_id:str, start_date:str, stop_date:str,
         name=f'Orbite {asteroid_id}'
     ))
 
-    fig.add_trace(go.Scatter3d(
-        x=[vectors['x'][0]*1.496e+8], y=[vectors['y'][0]*1.496e+8], z=[vectors['z'][0]*1.496e+8],
-        mode='markers+text',
-        marker=dict(size=4, color='white', opacity=0.9),
-        text=asteroid_id
-    ))
-
     return fig
+
+def get_neos_name(date_max:str, date_min:str, dist_max:str, fullname:str, nea_comet:str)->pd.DataFrame:
+    url = f'https://ssd-api.jpl.nasa.gov/cad.api?dist-max={dist_max}&date-min={date_min}&date-max={date_max}&fullname={fullname}&nea-comet={nea_comet}'
+    r = requests.get(url)
+    data = r.json()
+    columns = data['fields']
+    df = pd.DataFrame(data['data'], columns=columns)
+    return df
